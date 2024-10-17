59730a0896ad34b7285187c9f4d84cc4884b837d
Raghadsaad <zryr0022@gmail.com>
2024-10-18 02:51:15 +0300

    The first upload

---
+++ Assignment1/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        
+        Console.Write("Enter the second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        
+        Console.Write("Choose an operation (+, -, *, /): ");
+        string operation = Console.ReadLine();
+
+       
+        if (operation == "+")
+        {
+            Console.WriteLine("Result: " + (num1 + num2));
+        }
+        else if (operation == "-")
+        {
+            Console.WriteLine("Result: " + (num1 - num2));
+        }
+        else if (operation == "*")
+        {
+            Console.WriteLine("Result: " + (num1 * num2));
+        }
+        else if (operation == "/")
+        {
+            if (num2 != 0)
+            {
+                Console.WriteLine("Result: " + (num1 / num2));
+            }
+            else
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error: Invalid operation.");
+        }
+    }
+}
